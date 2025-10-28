f601b029d9c9e0c8b84d5c2676eb6721cd2a8855
bitri9shub <lamghari_ahme@outlook.fr>
2025-10-28 11:29:04 +0100

    head update

--- data_structures/intro_linked_lists/main/main.c
+++ data_structures/intro_linked_lists/main/main.c
@@ -10,43 +10,47 @@ typedef struct cell
 int main()
 {
     // Declaration des cellules
-    cellule* cell_1;
+    cellule* tete;
     cellule* cell_2;
     cellule* cell_3;
+    // cellule* tête; Aulieu de définir la tête ensuite la lier avec la premiere cellule,
+    // il suffit de prendre dès le départ la premiere cellule comme tête
 
     // Allocation de mémoire pour les cellules
-    cell_1 = (cellule*) malloc(sizeof(cellule));
+    tete = (cellule*) malloc(sizeof(cellule));
     cell_2 = (cellule*) malloc(sizeof(cellule));
     cell_3 = (cellule*) malloc(sizeof(cellule));
+    // tete = cell_1; // tete et cell_1 pointent vers le même bloc mémoire
 
     // Initialisation des variables pour les cellules
-    cell_1->value = 10;
+    tete->value = 10;
     cell_2->value = 20;
     cell_3->value = 110;
 
     // Initialisation des adresses vers les quelles pointes chaque cellule
-    cell_1->next = NULL;
+    tete->next = NULL;
     cell_2->next = NULL;
     cell_3->next = NULL;
 
     // Lier les cellules
-    cell_1->next = cell_2; // cell_2 retourne l'adresse du premier element de cell_2
+    tete->next = cell_2; // cell_2 retourne l'adresse du premier element de cell_2
     cell_2->next = cell_3; // cell_3 retourne l'adresse du premier element de cell_3
 
     // Affichage des cellules
-    printf("Affichage cellule 1\n");
-    printf("%d\n", cell_1->value);
-    printf("%p\n", cell_1->next);
+    printf("Affichage cellule 1:\n");
+    printf("valeur 1: %d\n", tete->value);
+    printf("adresse 1: %p\n", tete->next);
+    printf("\n");
 
-    printf("Affichage cellule 2\n");
-    printf("%d\n", cell_2->value);
-    printf("%p\n", cell_2->next);
+    printf("Affichage cellule 2:\n");
+    printf("valeur 2: %d\n", cell_2->value);
+    printf("adresse 2: %p\n", cell_2->next);
+    printf("\n");
 
-    printf("Affichage cellule 1\n");
-    printf("%d\n", cell_3->value);
-    printf("%p\n", cell_3->next);
-
-
+    printf("Affichage cellule 3:\n");
+    printf("valeur 3: %d\n", cell_3->value);
+    printf("adresse 3: %p\n", cell_3->next);
+    printf("\n");
 
     return 0;
 }
